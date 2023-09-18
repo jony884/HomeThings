84830f53c66e2e0de5db7e54060fa1600998376d
jony884 <139261229+jony884@users.noreply.github.com>
2023-09-18 18:48:22 +0900

    fix2

--- db/migrate/20230906030416_create_items.rb
+++ db/migrate/20230906030416_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration[7.0]
   def change
     create_table :items do |t|
       t.string :item_name
-      t.string :item_dscription
+      t.string :item_description
       t.timestamps
     end
   end
